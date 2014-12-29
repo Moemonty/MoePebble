33a495c43e29283da6476c2929b97c60b33f1c1c
Moises Montenegro <moemonty@gmail.com>
2014-12-29 15:20:55 -0600

    Updated watch font.
    Began implementing JS portion of tutorial here..http://developer.getpebble.com/getting-started/watchface-tutorial/part3/

--- src/main.c
+++ src/main.c
@@ -3,7 +3,19 @@ static Window *s_main_window;
 
 static TextLayer *s_time_layer;
 
+static TextLayer *s_weather_layer;
+
 static GFont s_time_font;
+
+//Loading Font
+static  GFont s_time_font;
+static GFont s_weather_font;
+
+
+
+static BitmapLayer *s_background_layer;
+
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   
@@ -24,19 +36,51 @@ static void update_time() {
   
 }
 
+
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
 
 
 static void main_window_load(Window *window) {
-  //load text layer
+  
+  //background bitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  
+  s_background_layer = bitmap_layer_create(GRect(0,0,144,168));
+  
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
+  
+  
+  //Open Data Weather Layer
+  s_weather_layer = text_layer_create(GRect(0, 120, 144, 25));
+  
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+
+  text_layer_set_text(s_weather_layer, "Moemonty");
+  
   
   s_time_layer = text_layer_create(GRect(5,52,139,50));
   
   text_layer_set_background_color(s_time_layer, GColorClear);
   
   text_layer_set_text_color(s_time_layer, GColorBlack);
+  
+  
+  //weahter text layer
+  
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  
   
   //text_layer_set_text(s_time_layer, "00:00");
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
@@ -52,9 +96,12 @@ static void main_window_load(Window *window) {
 }
 
 static void main_window_unload(Window *window) {
-
+    gbitmap_destroy(s_background_bitmap);
     text_layer_destroy(s_time_layer);
     fonts_unload_custom_font(s_time_font);
+    bitmap_layer_destroy(s_background_layer);
+    text_layer_destroy(s_weather_layer);
+    fonts_unload_custom_font(s_weather_font);
 }
   
 
